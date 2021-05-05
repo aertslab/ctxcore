e7cd6d022a75a0c69d0735e8a231d9a24dd2d816
Chris Flerin <ccflerin@gmail.com>
2021-05-05 16:11:47 +0200

    Add files for package structure

---
+++ setup.py
@@ -0,0 +1,62 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+regexp = re.compile(r'.*__version__ = [\'\"](.*?)[\'\"]', re.S)
+
+base_package = 'pycistarget_core'
+base_path = os.path.dirname(__file__)
+
+init_file = os.path.join(base_path, 'src', 'pycistarget_core', '__init__.py')
+with open(init_file, 'r') as f:
+    module_content = f.read()
+
+    match = regexp.match(module_content)
+    if match:
+        version = match.group(1)
+    else:
+        raise RuntimeError(
+            'Cannot find __version__ in {}'.format(init_file))
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+with open('CHANGELOG.rst', 'r') as f:
+    changes = f.read()
+
+def parse_requirements(filename):
+    ''' Load requirements from a pip requirements file '''
+    with open(filename, 'r') as fd:
+        lines = []
+        for line in fd:
+            line.strip()
+            if line and not line.startswith("#"):
+                lines.append(line)
+    return lines
+
+requirements = parse_requirements('requirements.txt')
+
+
+if __name__ == '__main__':
+    setup(
+        name='pycistarget_core',
+        description='Core functions for pycisTarget and the SCENIC tool suite',
+        long_description='\n\n'.join([readme, changes]),
+        license='GNU General Public License v3',
+        url='https://github.com/aertslab/pycistarget_core',
+        version=version,
+        author='Bram Van de Sande',
+        maintainer='Christopher Flerin',
+        maintainer_email='christopher.flerin@kuleuven.be',
+        install_requires=requirements,
+        keywords=['pycistarget_core'],
+        package_dir={'': 'src'},
+        packages=find_packages('src'),
+        zip_safe=False,
+        classifiers=['Development Status :: 3 - Alpha',
+                     'Intended Audience :: Developers',
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7',
+                     'Programming Language :: Python :: 3.8']
+    )
