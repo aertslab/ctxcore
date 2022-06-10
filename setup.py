475eb23f8156c52caeb42e46b1767853b2fb8d76
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2022-06-10 11:41:57 +0200

    Update setup.py.
    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,47 +1,50 @@
-import os
-
 from setuptools import find_packages, setup
 
-with open('README.rst', 'r') as f:
-    readme = f.read()
+with open("README.rst", mode="r", encoding="utf-8") as fh:
+    readme = fh.read()
 
-with open('CHANGELOG.rst', 'r') as f:
-    changes = f.read()
+with open("CHANGELOG.rst", mode="r", encoding="utf-8") as fh:
+    changes = fh.read()
+
 
 def parse_requirements(filename):
-    ''' Load requirements from a pip requirements file '''
-    with open(filename, 'r') as fd:
+    """Load requirements from a pip requirements file"""
+    with open(filename, mode="r", encoding="utf-8") as fh:
         lines = []
-        for line in fd:
+        for line in fh:
             line.strip()
             if line and not line.startswith("#"):
                 lines.append(line)
     return lines
 
-requirements = parse_requirements('requirements.txt')
+
+requirements = parse_requirements("requirements.txt")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(
-        name='ctxcore',
+        name="ctxcore",
         use_scm_version=True,
-        setup_requires=['setuptools_scm'],
-        description='Core functions for pycisTarget and the SCENIC tool suite',
-        long_description='\n\n'.join([readme, changes]),
-        license='GNU General Public License v3',
-        url='https://github.com/aertslab/ctxcore',
-        author='Bram Van de Sande',
-        maintainer='Christopher Flerin',
-        maintainer_email='christopher.flerin@kuleuven.be',
+        setup_requires=["setuptools_scm"],
+        description="Core functions for pycisTarget and the SCENIC tool suite",
+        long_description="\n\n".join([readme, changes]),
+        license="GNU General Public License v3",
+        url="https://github.com/aertslab/ctxcore",
+        author="Bram Van de Sande",
+        maintainer="Gert Hulselmans",
+        maintainer_email="gert.hulselmans@kuleuven.be",
         install_requires=requirements,
-        keywords=['ctxcore','cisTarget','pycisTarget','SCENIC','pySCENIC'],
+        keywords=["ctxcore", "cisTarget", "pycisTarget", "SCENIC", "pySCENIC"],
         include_package_data=True,
-        package_dir={'': 'src'},
-        packages=find_packages('src'),
+        package_dir={"": "src"},
+        packages=find_packages("src"),
         zip_safe=False,
-        classifiers=['Development Status :: 3 - Alpha',
-                     'Intended Audience :: Developers',
-                     'Programming Language :: Python :: 3.6',
-                     'Programming Language :: Python :: 3.7',
-                     'Programming Language :: Python :: 3.8']
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+        ],
     )
