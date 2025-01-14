2519681442b0246c5f0873e1f44ee149b2a8ab73
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2025-01-14 15:23:40 +0100

    Fix most issues reported by "ruff check" in "src/ctxcore/binarization.py".
    Fix most issues reported by "ruff check" in "src/ctxcore/binarization.py".

--- src/ctxcore/binarization.py
+++ src/ctxcore/binarization.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 from multiprocessing import Pool
-from typing import Mapping, Optional
+from typing import TYPE_CHECKING, Mapping
 
 import numpy as np
 import pandas as pd
@@ -8,25 +10,31 @@ from scipy import stats
 from scipy.optimize import minimize_scalar
 from sklearn import mixture
 
+if TYPE_CHECKING:
+    import numpy.typing as npt
+
 
 def derive_threshold(
     auc_mtx: pd.DataFrame,
     regulon_name: str,
-    seed: Optional[float] = None,
+    seed: float | None = None,
     method: str = "hdt",
 ) -> float:
     """
-    Derive threshold on the AUC values of the given regulon to binarize the cells in two clusters: "on" versus "off"
-    state of the regulator.
+    Derive threshold on the AUC values of the given regulon to binarize the cells in
+    two clusters: "on" versus "off" state of the regulator.
 
-    :param auc_mtx: The dataframe with the AUC values for all cells and regulons (n_cells x n_regulons).
+    :param auc_mtx: The dataframe with the AUC values for all cells and regulons
+        (n_cells x n_regulons).
     :param regulon_name: the name of the regulon for which to predict the threshold.
-    :param method: The method to use to decide if the distribution of AUC values for the given regulon is not unimodel.
+    :param method: The method to use to decide if the distribution of AUC values for
+        the given regulon is not unimodel.
         Can be either Hartigan's Dip Test (HDT) or Bayesian Information Content (BIC).
         The former method performs better.
-        The BIC compares the BIC for two Gaussian Mixture Models: single versus two components.
+        The BIC compares the BIC for two Gaussian Mixture Models: single versus two
+        components.
     :return: The threshold on the AUC values.
-    """
+    """  # noqa: D205
     assert auc_mtx is not None
     assert not auc_mtx.empty
     assert regulon_name in auc_mtx.columns
@@ -37,9 +45,10 @@ def derive_threshold(
     if seed:
         np.random.seed(seed=seed)
 
-    def isbimodal(data, method: str) -> bool:
+    def isbimodal(data: npt.NDarray, method: str) -> bool:
         if method == "hdt":
-            # Use Hartigan's dip statistic to decide if distribution deviates from unimodality.
+            # Use Hartigan's dip statistic to decide if distribution deviates from
+            # unimodality.
             _, pval = diptst(
                 np.sort(data, axis=0),
                 sort_x=False,
@@ -59,16 +68,18 @@ def derive_threshold(
             return gmm2.bic(X) <= gmm1.bic(X)
 
     if not isbimodal(data, method):
-        # For a unimodal distribution the threshold is set as mean plus two standard deviations.
+        # For an unimodal distribution the threshold is set as mean plus two standard
+        # deviations.
         return data.mean() + 2.0 * data.std()
     else:
-        # Fit a two component Gaussian Mixture model on the AUC distribution using an Expectation-Maximization algorithm
-        # to identify the peaks in the distribution.
+        # Fit a two component Gaussian Mixture model on the AUC distribution using an
+        # Expectation-Maximization algorithm to identify the peaks in the distribution.
         gmm2 = mixture.GaussianMixture(
             n_components=2, covariance_type="full", random_state=seed
         ).fit(data.reshape(-1, 1))
-        # For a bimodal distribution the threshold is defined as the "trough" in between the two peaks.
-        # This is solved as a minimization problem on the kernel smoothed density.
+        # For a bimodal distribution the threshold is defined as the "trough" in
+        # between the two peaks. This is solved as a minimization problem on the kernel
+        # smoothed density.
         return minimize_scalar(
             fun=stats.gaussian_kde(data), bounds=sorted(gmm2.means_), method="bounded"
         ).x[0]
@@ -76,20 +87,26 @@ def derive_threshold(
 
 def binarize(
     auc_mtx: pd.DataFrame,
-    threshold_overides: Optional[Mapping[str, float]] = None,
-    seed: Optional[float] = None,
-    num_workers=1,
+    threshold_overides: Mapping[str, float] | None = None,
+    seed: float | None = None,
+    num_workers: int = 1,
 ) -> (pd.DataFrame, pd.Series):
     """
-    "Binarize" the supplied AUC matrix, i.e. decide if for each cells in the matrix a regulon is active or not based
-    on the bimodal distribution of the AUC values for that regulon.
+    "Binarize" the supplied AUC matrix, i.e. decide if for each cells in the matrix a
+    regulon is active or not based on the bimodal distribution of the AUC values for
+    that regulon.
 
-    :param auc_mtx: The dataframe with the AUC values for all cells and regulons (n_cells x n_regulons).
-    :param threshold_overides: A dictionary that maps name of regulons to manually set thresholds.
-    :return: A "binarized" dataframe and a series containing the AUC threshold used for each regulon.
-    """
+    :param auc_mtx: The dataframe with the AUC values for all cells and regulons
+        (n_cells x n_regulons).
+    :param threshold_overides: A dictionary that maps name of regulons to manually set
+        thresholds.
+    :return: A "binarized" dataframe and a series containing the AUC threshold used for
+        each regulon.
+    """  # noqa: D205
 
-    def derive_thresholds(auc_mtx, seed=seed):
+    def derive_thresholds(
+        auc_mtx: pd.DataFrame, seed: float | None = seed
+    ) -> pd.Series:
         with Pool(processes=num_workers) as p:
             thrs = p.starmap(
                 derive_threshold, [(auc_mtx, c, seed) for c in auc_mtx.columns]
